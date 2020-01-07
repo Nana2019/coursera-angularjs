d3201817c7818b4168eeb7ca30129ddbff84d08f
Nana2019 <weina131109@gmail.com>
2020-01-07 14:31:00 -0500

    Assignment 1 done.

---
+++ Assignments/Assignment1/app.js
@@ -0,0 +1,20 @@
+(function (){
+  'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+  $scope.check = function () {
+    var string = $scope.dishes.split(',');
+    var valueOfStr = string.length;
+    if(valueOfStr <= 3) {
+      $scope.msg="Enjoy!";
+    }else {
+      $scope.msg = "Too much!";
+    }
+  };
+}
+})();
--- example/Lecture04/app.js
+++ example/Lecture04/app.js
@@ -1,5 +1,23 @@
-(function () {
+(function (){
+  'use strict';
 
-angular.module('myFirstApp', []);
+angular.module('NameCalculator', [])
+
+.controller('NameCalculatorController', function ($scope) {
+  $scope.name = "";
+  $scope.totalValue = 0;
+
+  $scope.displayNumeric = function () {
+    $scope.totalValue = calculatNumericForString($scope.name);
+  };
+
+  function calculatNumericForString(string) {
+    var totalStringValue = 0;
+    for (var i = 0; i < string.length; i++) {
+      totalStringValue += string.charCodeAt(i);
+    }
+    return totalStringValue;
+  }
+});
 
 })();
